7d6df8981528867e9857637f697daf1126bc82f0
Christophe <cnamoune@gmail.com>
2021-04-03 18:58:32 +0200

    fix error into index.html and modify copyright by adding with Js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,7 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalCloseBtn = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
+const copyright = document.querySelector(".copyrights");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -26,3 +27,7 @@ function launchModal() {
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+//insert copyright with automatically good actual year
+const year = new Date().getFullYear();
+copyright.textContent = "Copyright 2014 - " + year + ", GameOn Inc.";
